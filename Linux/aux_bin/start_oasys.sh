916a8e448489e74e683783af97cfb50e740b737a
Jannis Maier <jannismaier97@gmail.com>
2025-05-26 13:36:07 +0200

    removing home path

--- Linux/aux_bin/start_oasys.sh
+++ Linux/aux_bin/start_oasys.sh
@@ -2,4 +2,4 @@
 
 export MATPLOTLIBRC="$HOME/.oasys/miniconda3/lib/python3.7/site-packages/matplotlib/mpl-data/matplotlibrc"
 
-"$HOME/.oasys/miniconda3/bin/python" -m oasys.canvas --force-discovery >> /home/jny/oasys_debug.log 2>&1
+"$HOME/.oasys/miniconda3/bin/python" -m oasys.canvas --force-discovery
