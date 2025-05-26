7dcd594b4d73c3eb2d8e7111d419aff951dcbb8c
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-05-26 13:18:32 +0200

    remove hardcoded paths

--- Linux/aux_bin/start_oasys.sh
+++ Linux/aux_bin/start_oasys.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Running start_oasys.sh" >> /home/jny/oasys_debug.log
-echo "DISPLAY=$DISPLAY" >> /home/jny/oasys_debug.log
-
 export MATPLOTLIBRC="$HOME/.oasys/miniconda3/lib/python3.7/site-packages/matplotlib/mpl-data/matplotlibrc"
 
 "$HOME/.oasys/miniconda3/bin/python" -m oasys.canvas --force-discovery >> /home/jny/oasys_debug.log 2>&1
